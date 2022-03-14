ef92750dcb1b1be3aef12a808ef86d96998f7b60
Rocio Arjona <rocioarjona@MacBook-Pro-de-Rocio.local>
2022-03-14 17:44:45 +0100

    first VUE attempt

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <BasicSetup />
+    <p>{{ mytext }}</p>
+  </div>
+</template>
+<script>
+import { BasicSetup } from './components/BasicSetup.vue'
+
+export default {
+  components: {
+        BasicSetup,
+    },
+    data () {
+      return {
+      mytext: "my text here!",
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+ <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/> -->
---
+++ hello-vue/src/components/BasicSetup.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <p>{{ mytext }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+p {
+  font-size: 1.8rem;
+}
+</style>
